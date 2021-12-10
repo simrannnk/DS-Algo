adec6c510a52c69c25a98f0c955ed8fc60cc93fc
Simranjeet Kaur <ksimran1720@gmail.com>
2021-12-10 23:54:54 +0530

    update

--- Foundation/L006_Recursion/l003.java
+++ Foundation/L006_Recursion/l003.java
@@ -134,7 +134,46 @@ public static int findSum(int[] nums,int S,int ssf,int idx)
      
      return ans1+ans2;
  }
-https://practice.geeksforgeeks.org/problems/rat-in-a-maze-problem/1
+ 
+// https://practice.geeksforgeeks.org/problems/rat-in-a-maze-problem/1
+public static void paths(int sr,int sc,int dr,int dc,int[][] dir,String[] dirs,int[][] vis,String psf,ArrayList<String> res)
+    {
+        
+        if(sr==dr && sc==dc)
+        {
+            res.add(psf);
+            return ;
+        }
+      
+        vis[sr][sc]=0;
+
+        for(int d=0;d<dir.length;d++)
+        {
+            int r=sr+dir[d][0];
+            int c=sc+dir[d][1];
+            
+            if(r>=0 && c>=0 && r<=dr && c<=dc && vis[r][c]==1)
+            {
+                paths(r,c,dr,dc,dir,dirs,vis,psf+dirs[d],res);
+            }
+        }
+        vis[sr][sc]=1;
+        
+    }
+    
+    public static ArrayList<String> findPath(int[][] m, int n) {
+        // Your code here   
+        int[][] dir={{1,0},{0,-1},{0,1},{-1,0}};
+        String[] dirs={"D","L","R","U"};
+        ArrayList<String> res =new ArrayList<>();
+        if(m[0][0]==0 || m[n-1][n-1]==0)
+        {
+            ArrayList<String> base=new ArrayList<>();
+           return base;
+        }
+       paths(0,0,n-1,n-1,dir,dirs,m,"",res);
+        return res;
+    }
 
 
     public static void main(String[] args)
